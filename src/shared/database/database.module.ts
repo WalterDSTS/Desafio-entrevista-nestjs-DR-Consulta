f650f3f12c5b9b5162e21156cd61ffb9abbffe9a
Walter dos Santos Simoni <waltersantossimoni@gmail.com>
2023-08-29 18:18:59 -0300

    chore: change config service to process env

--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -1,25 +1,18 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Parkzone } from 'src/modules/parkzone/entities/parkzone.entity';
-import { Vehicle } from 'src/modules/vehicles/entities/vehicle.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.getOrThrow('MYSQL_HOST'),
-        port: configService.getOrThrow('MYSQL_PORT'),
-        username: configService.getOrThrow('MYSQL_USERNAME'),
-        password: configService.getOrThrow('MYSQL_PASSWORD'),
-        database: configService.getOrThrow('MYSQL_DATABASE'),
-        entities: [Parkzone, Vehicle],
-        autoLoadEntities: true,
-        synchronize: configService.getOrThrow('MYSQL_SYNCHRONIZE'),
-        logging: true,
-      }),
-      inject: [ConfigService],
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: 3306,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
     }),
   ],
 })
